90f7284d1bfff8978105204d7573e89b10fb7960
Sergei Sorokin <serebanyshka7@gmail.com>
2018-07-28 19:02:49 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  const  now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.url}`;
+  fs.appendFile('server.log', log + '\n', error => error && console.log(error));
+  next();
+});
+
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+hbs.registerHelper('sreamIt', text => text.toUpperCase());
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    title: 'home',
+    pageTitle: 'Home Page',
+    message: 'Some message',
+  })
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    title: 'about',
+    pageTitle: 'About Page',
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({error: 'Page not found!'});
+});
+app.listen(3000, () => {
+  console.log('Server is up to port 3000');
+});
